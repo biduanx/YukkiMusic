9b986059a3e563d7ade43f0c18c1dd251df0c963
zulpiii <zpskibidi@gmail.com>
2025-09-27 01:51:24 +0800

    Update config.py
    Signed-off-by: zulpiii <zpskibidi@gmail.com>

--- config/config.py
+++ config/config.py
@@ -114,10 +114,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheTeamVivek"
+    "SUPPORT_CHANNEL", "https://t.me/haipalkon"
 )  # Example:- https://t.me/TheTeamVivek
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TheTeamVk"
+    "SUPPORT_GROUP", "https://t.me/zplog"
 )  # Example:- https://t.me/TheTeamVk
 
 
